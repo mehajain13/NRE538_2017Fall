b264eff32e29feb99d43b205a3c8473dfa222510
OscarFHC <oscarhsnurene@gmaii.com>
2017-01-23 01:35:26 -0500

    Creating files for wk4 (Lecture 5)

---
+++ Week4_Ttest/NRE538_week4_hypotesting.r
@@ -0,0 +1,2 @@
+myrecord = read.csv("https://raw.githubusercontent.com/OscarFHC/NRE538_GSRA/master/NRE538_LabsTOGO/Week4_ttest/Oscar_10K.csv", 
+                    sep=",", header=T, comment.char="#")
