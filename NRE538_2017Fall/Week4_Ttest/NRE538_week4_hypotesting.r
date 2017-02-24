cfeb188b9e2a8cda9b251fb372c1af0d5d039b78
OscarFHC <oscarhsnurene@gmaii.com>
2017-02-24 01:17:34 -0500

    20170223
    finish lab 7 and 8.

--- NRE538_2017Fall/Week4_Ttest/NRE538_week4_hypotesting.r
+++
@@ -1,2 +0,0 @@
-myrecord = read.csv("https://raw.githubusercontent.com/OscarFHC/NRE538_GSRA/master/NRE538_LabsTOGO/Week4_ttest/Oscar_10K.csv", 
-                    sep=",", header=T, comment.char="#")
